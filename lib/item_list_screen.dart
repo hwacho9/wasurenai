de016d3fda25a941b1cc3cf29d924dd15235a533
hwacho9 <csh77776@gmail.com>
2024-12-29 14:14:11 +0900

    feat:card swiper

--- lib/item_list_screen.dart
+++ lib/item_list_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import '../models/situation.dart';
-import '../widgets/item_card.dart';
 
 class ItemListScreen extends StatefulWidget {
   final Situation situation;
@@ -20,32 +20,68 @@ class _ItemListScreenState extends State<ItemListScreen> {
     items = List.from(widget.situation.items);
   }
 
-  void _showItemCard(int index) {
+  void _showItemSwiper() {
+    if (items.isEmpty) {
+      // items가 비어 있는 경우 처리
+      return;
+    }
+
     showModalBottomSheet(
       context: context,
-      isScrollControlled: true, // 스크롤 제어 활성화
+      isScrollControlled: true,
       builder: (BuildContext context) {
         return FractionallySizedBox(
-          heightFactor: 1.0, // 전체 화면 높이로 설정
-          child: ItemCard(
-            item: items[index],
-            onCheckNext: () {
-              setState(() {
-                items[index].isChecked = true; // 현재 항목 체크
-              });
-              Navigator.pop(context);
-              if (index < items.length - 1) {
-                _showItemCard(index + 1); // 다음 항목으로 이동
-              }
-            },
-            onCheckPrevious: () {
-              setState(() {
-                items[index].isChecked = true; // 현재 항목 체크
-              });
-              Navigator.pop(context);
-              if (index > 0) {
-                _showItemCard(index - 1); // 이전 항목으로 이동
-              }
+          heightFactor: 1.0,
+          child: CardSwiper(
+            cardsCount: items.length,
+            numberOfCardsDisplayed: items.length < 2 ? items.length : 2,
+            cardBuilder:
+                (context, index, percentThresholdX, percentThresholdY) {
+              final item = items[index];
+              return Center(
+                // 카드 전체를 화면 가운데 정렬
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width *
+                      0.8, // 화면 너비의 80%로 카드 크기 설정
+                  height: MediaQuery.of(context).size.height *
+                      0.4, // 화면 높이의 40%로 카드 크기 설정
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    elevation: 4,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment:
+                            MainAxisAlignment.center, // 세로 방향 중앙 정렬
+                        crossAxisAlignment:
+                            CrossAxisAlignment.center, // 가로 방향 중앙 정렬
+                        children: [
+                          Text(
+                            item.name,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center, // 텍스트 내용도 중앙 정렬
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            item.location,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                            textAlign: TextAlign.center, // 텍스트 내용도 중앙 정렬
+                          ),
+                          SizedBox(height: 20),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
             },
           ),
         );
@@ -58,7 +94,7 @@ class _ItemListScreenState extends State<ItemListScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          widget.situation.name, // 상황 이름 표시
+          widget.situation.name,
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
@@ -66,51 +102,56 @@ class _ItemListScreenState extends State<ItemListScreen> {
         foregroundColor: Colors.black,
         elevation: 0,
       ),
-      body: Container(
-        color: Colors.grey[100],
-        child: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            final item = items[index];
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 2,
-                      blurRadius: 4,
-                      offset: Offset(0, 2), // 그림자 방향
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: ListTile(
-                  key: Key('$index'),
-                  title: Text(
-                    item.name,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    child: ListTile(
+                      key: Key('$index'),
+                      title: Text(
+                        item.name,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      subtitle: Text(
+                        item.location,
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                      ),
+                      trailing: Switch(
+                        value: item.isChecked,
+                        onChanged: (bool value) {
+                          setState(() {
+                            item.isChecked = value;
+                          });
+                        },
+                      ),
+                      onTap: _showItemSwiper, // 리스트 아이템 클릭 시 스와이퍼 표시
+                    ),
                   ),
-                  subtitle: Text(
-                    item.location,
-                    style: TextStyle(color: Colors.grey, fontSize: 14),
-                  ),
-                  trailing: Switch(
-                    value: item.isChecked,
-                    onChanged: (bool value) {
-                      setState(() {
-                        item.isChecked = value;
-                      });
-                    },
-                  ),
-                  onTap: () => _showItemCard(index), // 리스트 아이템 클릭 시 카드 표시
-                ),
-              ),
-            );
-          },
-        ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
